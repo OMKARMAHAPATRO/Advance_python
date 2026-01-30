3b3e59a13c291e6443e83d0a71752391fca9e1de
Omkar Mahapatro <mahapatroomkar@gmail.com>
2026-01-30 10:41:05 +0530

    file add and changes

---
+++ 1_operator.py
@@ -0,0 +1,57 @@
+a = int(input("Enter first number: "))
+b = int(input("enter the second number : "))
+# Arithmatic operators
+print("Arithmatic operators")
+print(f"sum : {a+b}")
+print(f"product : {a*b}")
+print(f"division : {a/b}")
+print(f"subtraction : {a-b}")
+print(f"floor division : {a//b}")
+print(f"modulus : {a%b}")
+print(f"exponent : {a**b} \n")
+
+# comparision operators
+print("Comparision operators")
+print(f"a equal to b : {a==b}")
+print(f"a not equal to b : {a!=b}")
+print(f"a greater than b : {a>b}")
+print(f"a less than b : {a<b}")
+print(f"a greater than or equal to b : {a>=b}")
+print(f"a less than or equal to b : {a<=b}\n")
+
+# logical opeator
+print("Logical operators")
+x = True
+y = False
+print(f"x and y : {x and y}")
+print(f"x or y : {x or y}")
+print(f"not x : {not x}\n")
+
+# Assignment operators
+print("Assignment operators")
+c = 5
+print(f"Initial value of c : {c}")
+c += 2
+print(f"c += 2 : {c}")
+c -= 2
+print(f"c -= 2 : {c}")
+c *= 2
+print(f"c *= 2 : {c}")
+c /= 2
+print(f"c /= 2 : {c}")
+c %= 2
+print(f"c %= 2 : {c}\n")
+
+# Bitwise operators
+print("Bitwise operators")
+p = 5  
+q = 3 
+print(f"p & q : {p & q}")  
+print(f"p | q : {p | q}") 
+print(f"p ^ q : {p ^ q}")
+print(f"~p : {~p}")
+print(f"p << 1 : {p << 1}")
+print(f"p >> 1 : {p >> 1}\n")
+
+
+
